e2ec74ee9a6183bab7f28588da8a04dbd37eda4a
luyunfei <luyunfei@bianlimall.com>
2018-11-14 15:44:46 +0800

    去余

--- src/router/index.js
+++ src/router/index.js
@@ -5,41 +5,10 @@ Vue.use(Router)
 
 export default new Router({
   routes: [{
-      path: '/',
-      name: 'login',
-      component: () =>
-        import('@/pages/login')
-    },
-    {
-      path: '/home',
-      name: 'home',
-      meta: [],
-      redirect: '/home/javaScript',
-      component: () =>
-        import('@/pages/home'),
-      children: [{
-          path: '/home/javaScript',
-          name: 'manage',
-          meta: ['javaScript'],
-          component: () =>
-            import('@/pages/manage/javaScript/javaScript')
-        },
-        {
-          path: '/home/java',
-          name: 'java',
-          meta: ['java'],
-          component: () =>
-            import('@/pages/manage/java/java')
-        },
-        {
-          path: '/home/vueEdit',
-          name: 'vueEdit',
-          meta: ['文本编辑'],
-          component: () =>
-            import('@/pages/vueEdit')
-        }
-      ]
-    },
-
-  ]
+    path: '/',
+    name: 'home',
+    meta: [],
+    component: () =>
+      import('@/pages/home'),
+  }, ]
 })
